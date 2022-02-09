88983b26b3688a8d57fa38c1ebf12748a5b11bed
Taha Almoayed <Almoayed24@gmail.com>
2022-02-09 20:17:03 +0100

    MVC.NET project
    Upload MVC.NET course graduation project

---
+++ MovieShop/Models/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieShop.Models
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        [Required, ]
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Actor { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
